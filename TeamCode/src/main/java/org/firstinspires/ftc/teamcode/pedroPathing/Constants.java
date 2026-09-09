f1477f2102895c9b0ad803a105820d53ff35a259
Wilson Brous <wils@gir0fa.com>
2026-09-09 16:15:07 -0500

    feat: update pedroPathing
    Signed-off-by: Gir0fa <wils@gir0fa.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,19 +1,61 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.bylazar.configurables.annotations.Configurable;
 
+@Configurable
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static PIDFCoefficients translationalPIDF = new PIDFCoefficients(0.1, 0, 0.004, 0.11);
+    public static FilteredPIDFCoefficients drivePIDF = new FilteredPIDFCoefficients(0.01, 0, 0.00001, 0.6, 0.06);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(13.29) // robot mass in kg
+            // calibrated 2-5-26
+            .forwardZeroPowerAcceleration(-34.582)
+            .lateralZeroPowerAcceleration(-65)
+            .translationalPIDFCoefficients(translationalPIDF)
+            .drivePIDFCoefficients(drivePIDF);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .leftFrontMotorName("frontLeft")
+            .rightFrontMotorName("frontRight")
+            .leftRearMotorName("backLeft")
+            .rightRearMotorName("backRight")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .useBrakeModeInTeleOp(true) // use brake mode
+            // calibrated 2-5-26
+            .xVelocity(77.597) // use forward velocity tuner
+            .yVelocity(60.498); // lateral velocity tuner (it goes to the left!)
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(5.5625) // inches
+            .strafePodX(-5.280) // inches
+            .hardwareMapName("odo") // from README.md
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED); // flipped for turret bot
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0.85, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
